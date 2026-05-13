89ae0a8e214fa848f78b637cbb19dd05c743bca8
mcele <19591224+MCele@users.noreply.github.com>
2026-05-13 13:57:04 -0300

    fix(chore): actualiza dependencia angular/animations

--- apps/backend/src/config/database.ts
+++ apps/backend/src/config/database.ts
@@ -2,10 +2,11 @@ import mongoose from 'mongoose';
 
 export async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/bioetica', {
+        await mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/evaluaciones-de-personal', {
             autoIndex: true,
             serverSelectionTimeoutMS: 5000,
         });
+        console.log('✅ Conectado a MongoDB => ', mongoose.connection.host + ':' + mongoose.connection.port + '/' + mongoose.connection.name);
 
     } catch (err) {
         console.error('❌ Error al conectar a MongoDB:', err);
